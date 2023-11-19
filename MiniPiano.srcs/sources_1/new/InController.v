8b3eb460fe45c1bf90088c9d046f105c18dfd6ef
Charley Xiao <59205189+Charley-xiao@users.noreply.github.com>
2023-11-19 11:52:07 +0800

    wow

---
+++ MiniPiano.srcs/sources_1/new/InController.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+module InController(
+    input [6:0] sel,
+    input [1:0] octave,
+    output reg [4:0] note
+);
+    reg [4:0] _oct;
+    always @(*) begin
+        assign _oct = (octave == 2'b00) ? 5'b00000 : (
+            (octave == 2'b01) ? 5'b00111 : 5'b01110
+        );  
+        case(sel)
+            7'b0000001: note = 5'b00000 + _oct;
+            7'b0000010: note = 5'b00001 + _oct;
+            7'b0000100: note = 5'b00010 + _oct;
+            7'b0001000: note = 5'b00011 + _oct;
+            7'b0010000: note = 5'b00100 + _oct;
+            7'b0100000: note = 5'b00101 + _oct;
+            7'b1000000: note = 5'b00110 + _oct;
+            default: note = 5'b11111;
+        endcase
+    end
+endmodule
